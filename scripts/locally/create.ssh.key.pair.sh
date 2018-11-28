0aaacf4372b08ce8846954d54ea87250adb278a3
Jérémie Litzler <webdev.jl@gmail.com>
2018-11-28 14:10:13 +0100

    Create create.ssh.key.pair.sh

---
+++ scripts/locally/create.ssh.key.pair.sh
@@ -0,0 +1,14 @@
+
+# $1 => user
+# $2 => IP address of server
+
+# Create the key pair
+ssh-keygen -t rsa -b 4096
+
+# Hit return on file name prompt
+# Type a passphrase with spaces
+
+# Copy securely the public key to the server
+scp /path_to_public_key/id_rsa.pub $1@$2:/home/$1/.ssh
+
+# Type the password
