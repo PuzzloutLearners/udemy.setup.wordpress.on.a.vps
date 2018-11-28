81f461d59dddcee30d48b16fdef57352e064ab85
Jérémie Litzler <webdev.jl@gmail.com>
2018-11-28 13:46:58 +0100

    Update check.updates.sh

--- scripts/check.updates.sh
+++ scripts/check.updates.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+# Download the packages to update
+sudo apt-get update
+
+# Install the packages
+sudo apt-get upgrade
+
+# If you see some packages that are listed as "The following packages have been kept back", it is because they need a server reboot
+# The following command to install those packages: 
+# sudo apt-get dist-upgrade
+# sudo reboot
+
+
 
