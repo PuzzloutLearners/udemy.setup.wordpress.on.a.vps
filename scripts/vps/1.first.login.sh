5e010b9f071f01627045e622d4618ace7f09dc92
Jérémie Litzler <webdev.jl@gmail.com>
2018-12-03 16:20:48 +0100

    Update 1.first.login.sh

--- scripts/vps/1.first.login.sh
+++ scripts/vps/1.first.login.sh
@@ -1,26 +1,54 @@
 #!/bin/bash
 
-# Add a new user
-adduser puzzlout
+# $1 => UNIX user to create
 
-# Check nano is installed to the latest version
+sudo apt-get update && apt-get install git
+
+git config --global credential.helper 'store --file ~/.my-credentials'
+git config --global user.name "Jeremie Litzler"
+git config --global user.email puzzlout@gmail.com
+
+###############################################################################
+## Server commons installs
+## 
+## Variable checks
+##
+###############################################################################
+if [[ $1 == "" ]]
+	then
+		printf "The unix user is required.\n"
+		exit 1;
+fi
+
+###############################################################################
+##
+## First login
+##
+###############################################################################
+echo "Add a new user"
+adduser $1
+
+echo "Check nano is installed to the latest version"
 apt-get install nano
-
-# Give the new user root privileges
-visudo
 
 # Under "User privilege specification", add the following line
 # puzzlout  ALL=(ALL:ALL) ALL
 #
 # CTRL + X then Y then Enter
+echo "Give the new user root privileges"
+visudo
 
-# Navigate the root dir
+echo "Navigate the /etc/ssh dir"
 cd /etc/ssh
-nano sshd_config
-# Under "Authentication", update Authentication to no
 
-# Restart ssh service
+# Under "Authentication", update Authentication to no
+echo "Edit the ssh conf"
+nano sshd_config
+
+echo "Restart ssh service"
 service ssh restart
 
-# Disconnect from Root
+echo "Disconnect from Root"
+logout
+
 # Try to connect from puzzlout
