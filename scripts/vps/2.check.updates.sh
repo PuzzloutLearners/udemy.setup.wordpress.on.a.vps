bb1224d81406790cec272117f31bf10520a27aaf
Jérémie Litzler <webdev.jl@gmail.com>
2018-12-03 16:24:22 +0100

    Update 2.check.updates.sh

--- scripts/vps/2.check.updates.sh
+++ scripts/vps/2.check.updates.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Download the packages to update
+###############################################################################
+## Server commons installs
+## 
+## Variable checks
+##
+###############################################################################
+echo "Download the packages to update"
 sudo apt-get update
 
-# Install the packages
+echo "Install the packages"
 sudo apt-get upgrade
 
-# If you see some packages that are listed as "The following packages have been kept back", it is because they need a server reboot
-# The following command to install those packages: 
-# sudo apt-get dist-upgrade
-# sudo reboot
-
-
-
+echo "If you see some packages that are listed as \"The following packages have been kept back\", it is because they need a server reboot \n"
+echo "The following command to install those packages: \n"
+echo "sudo apt-get dist-upgrade \n"
+echo "sudo reboot"
