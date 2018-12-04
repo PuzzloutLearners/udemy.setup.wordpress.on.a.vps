06ab7ad978cec8bfc76092d08df8ae2d9cd204c5
Jérémie Litzler <webdev.jl@gmail.com>
2018-12-04 14:30:03 +0100

    Update script.sh

--- scripts/vps/4.setup.firewall/script.sh
+++ scripts/vps/4.setup.firewall/script.sh
@@ -4,12 +4,12 @@ sudo iptables -L
 # No rules are active.
 
 sudo nano /etc/iptables.firewall.rules
-# Copy the content of firewall-rules.txt into "iptables.firewall.rules"
+# Copy the content of the asset file "firewall-rules.txt" into "iptables.firewall.rules"
 
 sudo iptables-restore < /etc/iptables.firewall.rules
 
 sudo nano /etc/network/if-pre-up.d/firewall
-# Copy the content of firewall-reboot.txt into the file "firewall"
+# Copy the content of the asset file "firewall-reboot.txt" into the file "firewall"
 sudo chmod +x /etc/network/if-pre-up.d/firewall
 
 exit
