9b244d588719aa9a46fe75b2dadf96bca9f3d713
Jérémie Litzler <webdev.jl@gmail.com>
2018-12-04 14:54:14 +0100

    Update 2.configure.apache.sh

--- scripts/vps/5.setup.apache2/2.configure.apache.sh
+++ scripts/vps/5.setup.apache2/2.configure.apache.sh
@@ -13,4 +13,23 @@ sudo cp security.conf security.conf.bak
 # Restart Apache
 sudo service apache2 restart
 
-cd
+cd ..
+
+# Backup the general conf file 
+sudo cp apache2.conf apache2.conf.bak
+
+# Edit the apache2.conf file
+sudo nano apache2.conf
+
+# Add the content of the asset file "mpm_prefork_module.conf" at the end of the apache2.conf file
+# At the block "<Directory /var/www/>", modify "AllowOverride" to "All" to enable using .htaccess files
+
+# Disable the auto index module (listing all the files is a security problem)
+sudo a2dismod autoindex -f
+# Enable the rewrite module
+sudo a2enmod rewrite
+
+# Restart Apache
+sudo service apache2 restart
+
+
