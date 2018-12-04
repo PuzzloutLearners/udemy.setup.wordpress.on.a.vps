5e147d40864bd1137201a2fa4522e418b67eeb9e
Jérémie Litzler <webdev.jl@gmail.com>
2018-12-04 14:13:47 +0100

    Create script.sh

---
+++ scripts/vps/4.setup.firewall/script.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+sudo iptables -L
+# No rules are active.
+
+sudo nano /etc/iptables.firewall.rules
+# Copy the content of firewall-rules.txt into "iptables.firewall.rules"
+
+sudo iptables-restore < /etc/iptables.firewall.rules
+
+sudo nano /etc/network/if-pre-up.d/firewall
+# Copy the content of firewall-reboot.txt into the file "firewall"
+sudo chmod +x /etc/network/if-pre-up.d/firewall
+
+exit
+
+# Reboot and run the following command:
+sudo iptables -L
+
+# The rules must be active.
