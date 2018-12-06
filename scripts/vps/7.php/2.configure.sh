055e52ef3ed24b0261317f14f5cc75e6e87cf867
Jérémie Litzler <webdev.jl@gmail.com>
2018-12-06 15:08:27 +0100

    Update 2.configure.sh

--- scripts/vps/7.php/2.configure.sh
+++ scripts/vps/7.php/2.configure.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 cd
+nano php.ini
+# Copy the content of the asset file "php.ini.optimised" in /etc/php/7.0/apache2
+sudo cp php.ini /etc/php/7.0/apache2/php.ini
 
+sudo service apache2 restart
