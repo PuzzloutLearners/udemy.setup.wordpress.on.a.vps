2e85fa95effd0728f0f7ccd523a33e2b97b2d0d2
Jérémie Litzler <webdev.jl@gmail.com>
2018-12-07 13:26:51 +0100

    Update install.sh

--- scripts/vps/8.fail2ban/install.sh
+++ scripts/vps/8.fail2ban/install.sh
@@ -1,1 +1,7 @@
 
+
+sudo apt-get install fail2ban
+cd /etc/fail2ban
+sudo cp jail.conf jail.local
+sudo cp /home/puzzlout/udemy.setup.wordpress.on.a.vps/scripts/vps/8.fail2ban/assets/jail.local jail.local
+sudo systemctl restart fail2ban
