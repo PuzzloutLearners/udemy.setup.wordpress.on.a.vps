378f512bae6f1d24c072b5f4fa9cdece2f7fe1a2
Jérémie Litzler <webdev.jl@gmail.com>
2018-12-10 13:33:15 +0100

    Update install.sh

--- scripts/vps/8.fail2ban/install.sh
+++ scripts/vps/8.fail2ban/install.sh
@@ -11,3 +11,8 @@ sudo cp /home/puzzlout/udemy.setup.wordpress.on.a.vps/scripts/vps/8.fail2ban/ass
 sudo systemctl restart fail2ban
 sudo cp defaults-debian.conf defaults-debian.conf.custom
 
+# If you use WP Scan, you will need to stop fail2ban service using the following commande
+# sudo service fail2ban stop
+#
+# Don't forget to restart it when you are done
+# sudo service fail2ban start
