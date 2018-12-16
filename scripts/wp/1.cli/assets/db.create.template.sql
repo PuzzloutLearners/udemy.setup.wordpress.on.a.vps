a3032a2471a76de7bfa52e1a5dac8b001fcb5055
Jérémie Litzler <contact@jeremielitzler.net>
2018-12-16 07:50:40 +0100

    fix: try to set name not clashing with sed...

--- scripts/wp/1.cli/assets/db.create.template.sql
+++ scripts/wp/1.cli/assets/db.create.template.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE dbname;
-GRANT ALL PRIVILEGES ON dbname.* TO 'dbusername'@'localhost' IDENTIFIED BY 'dbuserpwd';
-SHOW GRANTS FOR 'dbusername'@'localhost';
+CREATE DATABASE thisdatabase;
+GRANT ALL PRIVILEGES ON thisdatabase.* TO 'user'@'localhost' IDENTIFIED BY 'password';
+SHOW GRANTS FOR 'user'@'localhost';
 FLUSH PRIVILEGES;
