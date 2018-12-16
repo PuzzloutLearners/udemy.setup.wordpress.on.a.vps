c47d3543e948563e4506870666c80480f5e1576a
Jérémie Litzler <contact@jeremielitzler.net>
2018-12-16 07:46:07 +0100

    fix: update the variable to avoid replacing the pwd variable...

--- scripts/wp/1.cli/assets/db.create.template.sql
+++ scripts/wp/1.cli/assets/db.create.template.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE db_name;
-GRANT ALL PRIVILEGES ON db_name.* TO 'db_user'@'localhost' IDENTIFIED BY 'db_user_pwd';
-SHOW GRANTS FOR 'db_user'@'localhost';
+GRANT ALL PRIVILEGES ON db_name.* TO 'db_user_name'@'localhost' IDENTIFIED BY 'db_user_pwd';
+SHOW GRANTS FOR 'db_user_name'@'localhost';
 FLUSH PRIVILEGES;
