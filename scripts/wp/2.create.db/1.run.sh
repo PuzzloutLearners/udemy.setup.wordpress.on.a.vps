93c6ff878865dfb7ed2551850dfd516b94721ff9
JL52059332 <Jeremie.litzler@conduent.com>
2018-12-20 13:24:12 +0100

    refactor: use a single file Setting up the unix variables doesn't quite work as expected

--- scripts/wp/2.create.db/1.run.sh
+++ scripts/wp/2.create.db/1.run.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
+#!/bin/sh
+
+# Constants
+max_random_str_size_generic=6
+max_random_str_size_pwd=16
+
+defaultemail="puzzlout@gmail.com"
+
+echo $max_random_str_size_generic
+echo $max_random_str_size_pwd
+echo $defaultemail
+
+# Variables
 if [[ $1 == "" ]]
 	then
 		printf "The unix user is required.\n"
@@ -40,3 +52,33 @@ echo $dbuserpwd
 echo $wpadmuser
 echo $wpadmuserpwd
 echo $wptableprefix
+
+# Prepare the SQL file
+
+if [[ $3 == "" ]]
+	then
+		printf "Please provide a value to clone the git repo containing the installer.\n"
+		exit 1;
+fi
+vpsinstallerdir=$3
+
+wpdbpreparedir="wp.db.prepare"
+mkdir $wpdbpreparedir
+cd
+
+templatesqlfile="db.create.template.sql"
+projectsqlfile="db.create.$project.sql"
+
+cp $vpsinstallerdir/scripts/wp/2.create.db/assets/$templatesqlfile $wpdbpreparedir/$projectsqlfile
+
+cd $wpdbpreparedir
+
+sed -i -e 's:dbname:'$dbname':g' $projectsqlfile
+sed -i -e 's:dbusername:'$dbuser':g' $projectsqlfile
+sed -i -e 's:dbuserpwd:'$dbuserpwd':g' $projectsqlfile
+
+# Run the SQL file
+
+mysql -u $root_mysql_user -p < $projectsqlfile
+
+cd
