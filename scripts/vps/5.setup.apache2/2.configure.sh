9a51ddd98f100aeb7d4be627a2afa891a8647c02
Jérémie Litzler <contact@jeremielitzler.net>
2018-12-23 06:30:33 +0100

    fix: resolve the issue with configuring apache2

--- scripts/vps/5.setup.apache2/2.configure.sh
+++ scripts/vps/5.setup.apache2/2.configure.sh
@@ -17,18 +17,21 @@ cd /etc/apache2/conf-available/
 # Backup the security conf file 
 sudo cp security.conf security.conf.bak
 
-sudo cp /home/$username/$vpsinstaller/scripts/vps/5.setup.apache2/assets/security.conf security.conf
 # Modify the value "ServerTokens" from "OS" to "Prod".
-# Uncomment the line "ServerSignature off"
+sudo sed -i -e 's:ServerTokens OS:ServerTokens Prod:g' security.conf
 # Comment the line "ServerSignature on"
+sudo sed -i -e 's:ServerSignature On:#ServerSignature On:g' security.conf
+# Uncomment the line "ServerSignature off"
+sudo sed -i -e 's:#ServerSignature Off:ServerSignature Off:g' security.conf
+
 
 sudo service apache2 restart
 cd ..
 sudo cp apache2.conf apache2.conf.bak
-sudo cp /home/$username/$vpsinstaller/scripts/vps/5.setup.apache2/assets/apache2.conf apache2.conf
 # Add the content of the asset file "mpm_prefork_module.conf" at the end of the apache2.conf file
+sudo cat /home/$username/$vpsinstaller/scripts/vps/5.setup.apache2/assets/mpm_prefork_module.conf >> apache2.conf
 # At the block "<Directory /var/www/>", modify "AllowOverride" to "All" to enable using .htaccess files
-
+sudo nano apache2.conf
 # Disable the auto index module (listing all the files is a security problem)
 sudo a2dismod autoindex -f
 # Enable the rewrite module
