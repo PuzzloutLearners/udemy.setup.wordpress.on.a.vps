2850e7844c35eb95d08fce5243ecbb5a8c8eb054
JL52059332 <Jeremie.litzler@conduent.com>
2018-12-28 08:17:55 +0100

    fix: set git config

--- vps/step2.sh
+++ vps/step2.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+git config --global push.default simple
+git config --global credential.helper 'store --file ~/.my-credentials'
+git config --global user.name "Jeremie Litzler"
+git config --global user.email puzzlout@gmail.com
+
+
 repodir="vpsinstaller"
 bash $repodir/vps/3.secure.ssh.dir/script.sh 
 sudo cp $repodir/vps/3.secure.ssh.dir/assets/authorized_keys .ssh/
