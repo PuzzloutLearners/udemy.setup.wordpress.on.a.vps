231d86ca41d9157561ca9e630d5a54fd4b929161
JL52059332 <Jeremie.litzler@conduent.com>
2018-12-28 07:16:59 +0100

    feat: update the domains

--- web/reset.sh
+++ web/reset.sh
@@ -1,15 +1,15 @@
 sudo rm -R /var/www/*
-domain1="asteol.dev.puzzlout.com"
+domain1="asteoldev.puzzlout.com"
 sudo a2dissite $domain1
 sudo rm /etc/apache2/sites-available/$domain1.conf
 sudo rm /etc/apache2/sites-available/$domain1-le-ssl.conf
 
-domain2="asteol.training.puzzlout.com"
+domain2="asteolformation.puzzlout.com"
 sudo a2dissite $domain2
 sudo rm /etc/apache2/sites-available/$domain2.conf
 sudo rm /etc/apache2/sites-available/$domain2-le-ssl.conf
 
-domain3="asteol.mockup.puzzlout.com"
+domain3="asteolstaging.puzzlout.com"
 sudo a2dissite $domain3
 sudo rm /etc/apache2/sites-available/$domain3.conf
 sudo rm /etc/apache2/sites-available/$domain3-le-ssl.conf
--- web/sites.setup.sh
+++ web/sites.setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 repodir=vpsinstaller
 username="puzzlout"
-domain1="asteol.dev.puzzlout.com"
+domain1="asteoldev.puzzlout.com"
 bash $repodir/web/create.new.site.conf.sh $domain1 $repodir $username
 bash $repodir/web/enable.ssl.sh $domain1
 echo "Try to view https://$domain1 in the browser!"
 
-domain2="asteol.training.puzzlout.com"
+domain2="asteolformation.puzzlout.com"
 bash $repodir/web/create.new.site.conf.sh $domain2 $repodir $username
 bash $repodir/web/enable.ssl.sh $domain3
 echo "Try to view https://$domain2 in the browser!"
 
-domain3="asteol.mockup.puzzlout.com"
+domain3="asteolstaging.puzzlout.com"
 bash $repodir/web/create.new.site.conf.sh $domain3 $repodir $username
 bash $repodir/web/enable.ssl.sh $domain3
 echo "Try to view https://$domain3 in the browser!"
